3a4a8b626da9b4bda3531d2c879836407f71a3fa
devyn <devyn.cairns@gmail.com>
2011-02-19 17:42:33 -0800

    DSL mode server factory (Ruby)

---
+++ Ruby/dr2/server/dsl.rb
@@ -0,0 +1,53 @@
+require 'dr2/server/base'
+require 'dr2/types/all'
+
+module Dr2
+  module Server
+    # Server, DSL-style.
+    class DSL < Base
+      class Context
+        attr_accessor :nodes
+
+        def initialize
+          @nodes = {}
+        end
+
+        def node(name, &blk)
+          @nodes[name.to_s] = blk
+        end
+      end
+
+      def initialize(bind, port, &blk)
+        super bind, port
+        @ctx = Context.new
+        @ctx.instance_eval &blk
+      end
+
+      def receive(m, io, l=nil)
+        if m.is_a?(Dr2::Data::Error)
+          warn m.to_s
+        else
+          begin
+            case m.to
+            when nil
+              if n = @ctx.nodes[m.node]
+                r = n[*m.args]
+                l.lock if l
+                Dr2.write(io, Dr2::Data::Response.new(m.id, r))
+                l.unlock if l
+              else
+                raise Dr2::Data::Error, "NodeNotFound", "node not found on #root"
+              end
+            else
+              raise Dr2::Data::Error, "ReceiverNotFound", "receiver ##{m.to} not found"
+            end
+          rescue Exception
+            l.lock if l
+            Dr2.write(io, Dr2::Data::Response.new(m.id, $!))
+            l.unlock if l
+          end
+        end
+      end
+    end
+  end
+end
